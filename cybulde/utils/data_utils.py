185f812cb9c3b1592f95d7351a163f3637ef61e2
jalajrastogi4 <jalajrastogi4@gmail.com>
2024-03-27 14:02:28 +0530

    added initialization for DVC storage

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -12,9 +12,18 @@ def initialize_dvc():
     if is_dvc_initialized():
         DATA_UTILS_LOGGER.info("DVC is already initialized")
         return
-    DATA_UTILS_LOGGER.info("Initializing DVC")
+    DATA_UTILS_LOGGER.info("Initializing DVC...")
     run_shell_command("dvc init")
     run_shell_command("dvc config core.analytics false")
     run_shell_command("dvc config core.autostage true")
     run_shell_command("git add .dvc")
     run_shell_command("git commit -nm 'Initialized DVC'")
+
+def initialize_dvc_storage(dvc_remote_name, dvc_remote_url):
+    if not run_shell_command("dvc remote list"):
+        DATA_UTILS_LOGGER.inf("Initializing DVC storage...")
+        run_shell_command(f"dvc remote add -d {dvc_remote_name} {dvc_remote_url}")
+        run_shell_command("git add .dvc/config")
+        run_shell_command(f"git commit -nm 'Configured remotr storage at : {dvc_remote_url}'")
+    else:
+        DATA_UTILS_LOGGER.inf("DVC storage was already initialized")
